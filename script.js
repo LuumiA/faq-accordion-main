8b9dd512ef8071c65b5ec3edf7afb96afef7032d
LuumiA <nicolas.cava13@gmail.com>
2025-07-17 03:47:33 +0200

    Ajout de la logique pour ouvrir et fermer les réponses lors du clic sur une question

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 // @ts-nocheck
 const buttons = document.querySelectorAll(".question button");
+const answers = document.querySelectorAll(".answer");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const li = button.closest("li");
     const answer = li.querySelector(".answer");
-    console.log(answer);
+    const openOrClose = answer.classList.contains("open");
+    answers.forEach((answer) => {
+      answer.classList.remove("open");
+    });
+    if (!openOrClose) {
+      answer.classList.add("open");
+    }
   });
 });
