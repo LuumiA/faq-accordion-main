875b23f2d5005e570f5bfde9796a6db466663dd3
LuumiA <nicolas.cava13@gmail.com>
2025-12-16 16:14:14 +0100

    Ajout de la gestion des icônes pour l'affichage des états ouverts et fermés dans la section FAQ

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 // @ts-nocheck
 const button = document.querySelectorAll(".question");
 const items = document.querySelectorAll(".itemFaq");
+const icon = document.querySelectorAll(".icon");
 
 button.forEach((btn) => {
   btn.addEventListener("click", () => {
     // 1. Récupérer l'itemFaq parent de ce bouton
     const itemFaq = btn.closest(".itemFaq");
-
-    // 2. Vérifier si cet item est déjà "ouvert"
+    // 2. Récupère l’icône liée au bouton cliqué
+    const currentIcon = itemFaq.querySelector(".icon");
+    // 3. Vérifier si cet item est déjà "ouvert"
     const isOpen = itemFaq.classList.contains("isOpen");
 
-    // 3. Si l'item est déjà ouvert :
+    // 4. Si l'item est déjà ouvert :
     if (isOpen) {
       itemFaq.classList.remove("isOpen");
+      currentIcon.src = "assets/images/icon-plus.svg";
+      return;
     } else {
       items.forEach((item) => {
         item.classList.remove("isOpen");
+        const otherIcon = item.querySelector(".icon");
+        item.classList.remove("isOpen");
+        if (otherIcon) {
+          otherIcon.src = "assets/images/icon-plus.svg";
+        }
       });
       itemFaq.classList.add("isOpen");
+      currentIcon.src = "assets/images/icon-minus.svg";
     }
   });
 });
